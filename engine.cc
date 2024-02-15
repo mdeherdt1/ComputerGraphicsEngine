23464f3e1a4ab34b4d7477c9666abe74f776c9b2
mdeherdt1 <mathis.deherdt@student.uantwerpen.be>
2024-02-15 20:45:13 +0100

    EYE is working

--- engine.cc
+++ engine.cc
@@ -63,8 +63,7 @@ img::EasyImage Blocks(const string &type, const int &w, const int &h, const ini:
     return image;
 }
 
-img::EasyImage QuarterCircle(const string fig, const ini::DoubleTuple backGroundColor, const ini::DoubleTuple lineColor, int nrLines, const int height, const int width) {
-    if (fig == "QuarterCircle") {
+img::EasyImage QuarterCircle(const ini::DoubleTuple backGroundColor, const ini::DoubleTuple lineColor, int nrLines, const int height, const int width) {
         nrLines = nrLines - 1;
         int Hs = height / nrLines; //hoogte van het rechthoekje
         int Ws = width / nrLines; //breedte van het rechthoekje
@@ -79,10 +78,10 @@ img::EasyImage QuarterCircle(const string fig, const ini::DoubleTuple backGround
         int y1 = height - 1;
         while (counter <= nrLines) {
             if (x1 == height){
-                x1 = y1;
+                x1 = height - 1;
             }
             if (y0 == height){
-                y0 = y1;
+                y0 = height - 1;
             }
 
             if (make_tuple(x0, y0) == make_tuple(x1, y1)) {
@@ -99,7 +98,85 @@ img::EasyImage QuarterCircle(const string fig, const ini::DoubleTuple backGround
         }
         return image;
     }
- }
+
+
+
+img::EasyImage Eye(ini::DoubleTuple backGroundColor, ini::DoubleTuple lineColor, int nrLines, int height, int width) {
+    nrLines = nrLines - 1;
+    int Hs = height / nrLines; //hoogte van het rechthoekje
+    int Ws = width / nrLines; //breedte van het rechthoekje
+
+
+    img::EasyImage image(width, height,
+                         img::Color(backGroundColor[0] * 255, backGroundColor[1] * 255, backGroundColor[2] * 255));
+    //eerste (linkse)
+    int counter = 0;
+    int x0 = 0;
+    int y0 = 0;
+    int x1 = 0;
+    int y1 = height - 1;
+
+
+
+    while (counter <= nrLines) {
+        if (x1 == width){
+            x1 = width - 1;
+        }
+        if (y0 == height){
+            y0 = height - 1;
+        }
+
+        if (make_tuple(x0, y0) == make_tuple(x1, y1)) {
+            x0 += Hs;
+            y1 += Ws;
+
+        } else {
+            image.draw_line(x0, y0, x1, y1, img::Color(lineColor[0] * 255, lineColor[1] * 255, lineColor[2] * 255));
+            y0 += Hs;
+            x1 += Ws;
+        }
+
+
+        counter++;
+    }
+
+
+//    tweede (rechtse)
+    int counter2 = 0;
+    const int y2 = 0;
+    const int x3 = width -1;
+
+    int x2 = 0;
+    int y3 = 0;
+
+    while (counter2 <= nrLines) {
+        if (x2 == width){
+            x2 = width - 1;
+        }
+        if (y3 == height){
+            y3 = height - 1;
+        }
+
+        if (make_tuple(x2, y2) == make_tuple(x3, y3)) {
+            x2 += Ws;
+            y3 += Hs;
+
+
+        } else {
+            image.draw_line(x2, y2, x3, y3, img::Color(lineColor[0] * 255, lineColor[1] * 255, lineColor[2] * 255));
+            x2 += Ws;
+            y3 += Hs;
+        }
+
+
+        counter2++;
+    }
+
+    return image;
+}
+
+
+
 img::EasyImage generate_image(const ini::Configuration &confg)
 {
 
@@ -127,14 +204,14 @@ img::EasyImage generate_image(const ini::Configuration &confg)
         ini::DoubleTuple lineColor= confg["LineProperties"]["lineColor"];
         int nrLines = confg["LineProperties"]["nrLines"];
         if(figure == "QuarterCircle"){
-            return QuarterCircle(figure, backGroundColor, lineColor, nrLines, height, width);
+            return QuarterCircle(backGroundColor, lineColor, nrLines, height, width);
+        }
+        if(figure == "Eye"){
+            return Eye(backGroundColor, lineColor, nrLines, height, width);
         }
     }
     return image;
 }
-
-
-
 
 
 
